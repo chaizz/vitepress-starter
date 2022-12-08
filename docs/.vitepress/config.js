9d984e48c24f0c7148a28985e9d63c3ccecab9fe
chaizz <18037731392@163.com>
2022-12-08 09:44:41 +0800

    修改侧边栏

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,21 +3,23 @@ module.exports = {
     description: '世界上最大的游戏在线俱乐部~',
     themeConfig: {
         logo: "/logo.svg",
-        nav: [
-            {
-                text: "chaizz", link: '/about/'
-            },
-            {
-                text: "关于",
-                link: '/about/'
-            }
-        ],
+
+        nav: [{text: "about", link: '/about/'}],
+
         sidebar: [
             {
-                text: "web",
+                text: "css",
                 items: [
-                    {text: "css", link: "/Web/CSS/CSS媒体查询/"},
-                    {text: "js", link: "/Web/javascript/js笔记之js事件/"},
+                    {text: "CSS媒体查询", link: "/Web/CSS/CSS媒体查询/"},
+                    {text: "CSS层叠与继承", link: "/Web/CSS/CSS层叠与继承/"},
+                ],
+            },
+
+            {
+                text: "js",
+                items: [
+                    {text: "js笔记之js事件", link: "/Web/Javascript/js笔记之js事件/"},
+                    {text: "js笔记之Js代码调用策略", link: "/Web/Javascript/js笔记之Js代码调用策略/"},
                 ],
             }
         ],

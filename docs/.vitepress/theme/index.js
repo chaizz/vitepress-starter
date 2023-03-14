d71a74bf92096e2f9ac83d0e063aa60745e79756
chaizz <18037731392@163.com>
2023-03-14 20:34:29 +0800

    修改主题配置

---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,13 @@
+// import Theme from 'vitepress/theme'
+// import './style/var.css'
+
+// export default {
+//   ...Theme
+// }
+
+
+
+import DefaultTheme from 'vitepress/theme'
+import './style/var.css'
+
+export default DefaultTheme
